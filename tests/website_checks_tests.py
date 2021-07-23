5df1aed4bb114ee7e40e69c47ed4ded6ea078d28
roman <warrior7089@gmail.com>
2021-07-23 18:07:31 +0300

    added tests for http latency check

--- tests/website_checks_tests.py
+++ tests/website_checks_tests.py
@@ -2,15 +2,25 @@ import unittest
 
 from utils.constants import TestStatus
 from website_tests.dns.tests import resolve
+from website_tests.http.tests import latency
 
 
 class DnsChecksTests(unittest.TestCase):
     def test_dns(self):
         res = resolve('google.com')
-        self.assertEquals(TestStatus.SUCCESS, res.status)
+        self.assertEqual(TestStatus.SUCCESS, res.status)
         res = resolve('goosdfsfgle.com')
-        self.assertEquals(TestStatus.FAILURE, res.status)
+        self.assertEqual(TestStatus.FAILURE, res.status)
+
+
+class HttpChecksTests(unittest.TestCase):
+    def test_http_latency(self):
+        res = latency('google.com')
+        self.assertEqual(TestStatus.SUCCESS, res.status)
+        res = resolve('goosdfsfgle.com')
+        self.assertEqual(TestStatus.FAILURE, res.status)
 
 
 if __name__ == '__main__':
     unittest.main()
+
