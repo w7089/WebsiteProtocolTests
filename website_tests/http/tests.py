516e814ee041d360f26b2afc2062cf8758dce8e9
roman <warrior7089@gmail.com>
2021-07-24 14:53:10 +0300

    added custom configuration to http test

--- website_tests/http/tests.py
+++ website_tests/http/tests.py
@@ -1,12 +1,18 @@
+import time
+
 import requests
 
 from utils.constants import TestResult, TestStatus
 
 
-def latency(website_domain):
-    res = requests.get(f"http://{website_domain}")
-    if res.status_code == 200:
-        lat = res.elapsed.total_seconds()
-        return TestResult(status=TestStatus.SUCCESS, info=f'latency: {lat} seconds', val=lat)
-    else:
+def latency(website_domain, runs=1, wait_between_run=0):
+    latencies = list()
+    for _ in range(runs):
+        res = requests.get(f"http://{website_domain}")
+        if res.status_code == 200:
+            latencies.append(res.elapsed.total_seconds())
+        time.sleep(wait_between_run)
+    if not latencies:
         return TestResult()
+    lat = round(sum(latencies) / len(latencies), 6)
+    return TestResult(status=TestStatus.SUCCESS, info=f'latency: {lat} seconds', val=lat)
